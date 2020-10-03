cf00f5916f871bd3fc2e1f89b7a8e8c28c8ca68d
joaoalc <up201806531@fe.up.pt>
2020-10-03 22:39:21 +0100

    Added torus to parser branch

--- TP1/main.js
+++ TP1/main.js
@@ -12,7 +12,7 @@ function getUrlVars() {
 }	 
 //Include additional files here
 serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 'MyInterface.js', 'MyRectangle.js',
-                 'MyTriangle.js', 'MySphere.js', 'MyCylinder.js', 'MyNode.js',
+                 'MyTriangle.js', 'MySphere.js', 'MyCylinder.js', 'MyNode.js', 'MyTorus.js',
 
 main=function()
 {
