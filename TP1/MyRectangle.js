1793401cce27d9c2640f39632102b498eb5ec918
Davide Castro <up201806512@fe.up.pt>
2020-10-24 13:46:59 +0100

    Fixed rectangle amplification and changed amplifications in scene

--- TP1/MyRectangle.js
+++ TP1/MyRectangle.js
@@ -53,10 +53,10 @@ class MyRectangle extends CGFobject {
         */
 
 		this.texCoords = [
-			0, 1 / this.aft,
-			1 / this.afs, 1 / this.aft,
+			0, Math.abs((this.y2-this.y1)) / this.aft,
+			Math.abs((this.x2-this.x1)) / this.afs, Math.abs((this.y2-this.y1)) / this.aft,
 			0, 0,
-			1 / this.afs, 0
+			Math.abs((this.x2-this.x1)) / this.afs, 0
 		]
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
