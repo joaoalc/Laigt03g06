32e2f7c3f7e1717637d6429391a8f942a254c547
Davide Castro <up201806512@fe.up.pt>
2020-11-09 11:44:40 +0000

    Fixed Plane primitive nurbs evaluator

--- TP2/src/Plane.js
+++ TP2/src/Plane.js
@@ -5,17 +5,21 @@ class Plane extends CGFobject {
         this.uDivs = uDivs;
         this.vDivs = vDivs;
 
-        this.evaluator = {
-            divsU : uDivs,
-            divsV : vDivs,
-            getPoint : function(u, v) {
-                var x = -0.5 + u;
-                var y = 0;
-                var z = 0.5 - v;
+        //[[-0.5,0,-0.5], [-0.5,0,0.5], [0.5,0,0.5], [0.5,0,-0.5]]
 
-                return [x,y,z];
-            }
-        }
+        var controlPoints = [ [   [-0.5,0,-0.5,1], [0.5,0,-0.5,1]   ] , [   [-0.5,0,0.5,1], [0.5,0,0.5,1]   ]  ];
+        this.evaluator = new CGFnurbsSurface(1, 1, controlPoints);
+        // = {
+        //     divsU : uDivs,
+        //     divsV : vDivs,
+        //     getPoint : function(u, v) {
+        //         var x = -0.5 + u;
+        //         var y = 0;
+        //         var z = 0.5 - v;
+
+        //         return [x,y,z];
+        //     }
+        // }
         this.nurbs = new CGFnurbsObject(scene, uDivs, vDivs, this.evaluator);
     }
 
