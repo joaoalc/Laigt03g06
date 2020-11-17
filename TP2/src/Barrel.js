de289c2f2cf34af335f665261d27efd22c90092e
Davide Castro <up201806512@fe.up.pt>
2020-11-17 13:16:40 +0000

    Added animations to bell and wheels. Cleaned barrel code

--- TP2/src/Barrel.js
+++ TP2/src/Barrel.js
@@ -4,14 +4,8 @@ class Barrel {
         this.base = base;
         this.middle = middle;
         this.height = height;
-        this.npartsU = stacks;
-        this.npartsV = slices;
-
-//[[[-0.5, 0, -0.5, 1], [0, 0, -0.5, 1], [0.5, 0, -0.5, 1]], [[-0.5, 0, -0.5, 1], [0, 0, -0.5, 1], [0.5, 0, -0.5, 1]]]
-        var height_2 = this.height/2;
-
-        var c = 0.551915024494;
-        this.C = c;
+        this.npartsU = slices;
+        this.npartsV = stacks;
 
         var H = (4/3)*(middle-base);
 
@@ -44,30 +38,6 @@ class Barrel {
                 [-(base + H), 0, height - (H/tanAngle), 1],
                 [-base, 0, height, 1]
             ]
-            // [
-            //     [base, 0, 0, 1],
-            //     [(base + H), 0, H/tanAngle, 1],
-            //     [(base + H), 0, height - (H/tanAngle), 1],
-            //     [base, 0, height, 1]
-            // ],
-            // [
-            //     [base, h, 0, 1],
-            //     [(base + H), h, H/tanAngle, 1],
-            //     [(base + H), h, height - (H/tanAngle), 1],
-            //     [base, h, height, 1]
-            // ],
-            // [
-            //     [-base, h, 0, 1],
-            //     [-(base + H), h, H/tanAngle, 1],
-            //     [-(base + H), h, height - (H/tanAngle), 1],
-            //     [-base, h, height, 1]
-            // ],
-            // [
-            //     [-base, 0, 0, 1],
-            //     [-(base + H), 0, H/tanAngle, 1],
-            //     [-(base + H), 0, height - (H/tanAngle), 1],
-            //     [-base, 0, height, 1]
-            // ] 
         ];
 
         this.evaluator = new CGFnurbsSurface(3, 3, this.controlPoints);
@@ -86,8 +56,6 @@ class Barrel {
 
     display() {
         this.scene.pushMatrix();
-        
-        //this.scene.translate(this.C/2, 0, 0);
 
         this.nurbs.display();
 
