9da6186af82e7076829414ff60bf04d60196871b
Davide Castro <up201806512@fe.up.pt>
2020-11-17 23:25:58 +0000

    Added furnace spritesheet, textures and working clock to the tower

--- TP2/src/Plane.js
+++ TP2/src/Plane.js
@@ -7,7 +7,7 @@ class Plane extends CGFobject {
 
         //[[-0.5,0,-0.5], [-0.5,0,0.5], [0.5,0,0.5], [0.5,0,-0.5]]
 
-        var controlPoints = [ [   [-0.5,0,-0.5,1], [0.5,0,-0.5,1]   ] , [   [-0.5,0,0.5,1], [0.5,0,0.5,1]   ]  ];
+        var controlPoints = [ [  [-0.5,0,0.5,1], [-0.5,0,-0.5,1]  ] , [  [0.5,0,0.5,1],  [0.5,0,-0.5,1] ]  ];
         this.evaluator = new CGFnurbsSurface(1, 1, controlPoints);
         
         this.nurbs = new CGFnurbsObject(scene, uDivs, vDivs, this.evaluator);
