4c4b156355b5d1d80cf973d90b18798f2fd2eb37
Davide Castro <up201806512@fe.up.pt>
2021-01-03 22:28:36 +0000

    Fixed piece

--- TP3/src/game/MyScorePiece.js
+++ TP3/src/game/MyScorePiece.js
@@ -5,10 +5,12 @@ class MyScorePiece extends MyPiece {
 
     display(colours) {
         colours[this.colour].bind();
+        this.scene.pushMatrix();
         this.cylinder.display();
         this.scene.translate(0, 0, 0.2);
         this.scene.rotate(this.symbolRot, 0,0,1);
         this.symbol.display();
+        this.scene.popMatrix();
         colours[this.colour].unbind();
     }
 }
